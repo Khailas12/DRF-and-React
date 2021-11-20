904b79522d46dd2874aeec57cf181f465dba0ada
Khailas R <khailas303@gmail.com>
2021-11-20 19:24:20 +0530

    footer final

--- React/blog_api/src/index.js
+++ React/blog_api/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
 
