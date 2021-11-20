61554b54edbe3b5f5375244ab041cb605aa850a9
Khailas R <khailas303@gmail.com>
2021-11-20 16:13:18 +0530

    test passed (bugs on code fixed)

--- blog/tests.py
+++ blog/tests.py
@@ -6,45 +6,40 @@ from blog.models import Post, Category
 class TestCreatePost(TestCase):
     
     @classmethod
-    def setup_test_data(cls):
-        test_category = Category.objects.create(name='initial')
-        test_user_1 = User.objects.create_user(
-            username='user_1', password='12345678'
-        )
-        test_user_1.save()
+    def setUpTestData(cls):
+        test_category = Category.objects.create(name='main')
+
+        testuser1 = User.objects.create_user(
+            username='test_user', password='123456789')
+        testuser1.save()
+
         
         test_post = Post.objects.create(
             category_id=1,
             title='hello world',
-            excerpt='post excerpt',
-            content='con',
-            slug='title',
+            excerpt='Post Excerpt',
+            content='Gotham is great',
+            slug='some-title',
             author_id=1,
-            status='published'
+            status='published',
         )
         test_post.save()
         
-    def test_blog_content(self, *args, **kwargs):
+    
+    def test_blog_content(self):
+        post = Post.post_objects.get(id=1)
+        category = Category.objects.get(id=1)
         
-        try:
-            post = Post.post_objects.get(id=1)
-            category = Category.objects.get(id=1)
-        except Post and Category.DoesNotExist:
-            post = None
-            category = None
-
-            author = f'{post.author}'
-            excerpt = f'{post.excerpt}'
-            title = f'{post.title}'
-            content = f'{post.content}'
-            status = f'{post.status}'
-            
-            
-        # making sure that the data ensured is the actual data is inserted into the table
-        self.assertEqual(author, 'user_1')
+        author = f'{post.author}'
+        excerpt = f'{post.excerpt}'
+        title = f'{post.title}'
+        content = f'{post.content}'
+        status = f'{post.status}'
+        
+        self.assertEqual(author, 'test_user')
         self.assertEqual(title, 'hello world')
-        self.assertEqual(content, 'con')
+        self.assertEqual(content, 'Gotham is great')
         self.assertEqual(status, 'published')
         
         self.assertEqual(str(post), 'hello world')
-        self.assertEqual(str(category), 'initial')
+        self.assertEqual(str(category), 'main')
