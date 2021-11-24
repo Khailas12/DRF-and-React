e838848d508849b8d4a79119f34efead5a2389a8
Khailas R <khailas303@gmail.com>
2021-11-24 15:43:02 +0530

    initial commit

---
+++ react/blog_api/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        borderBotton: `1px solid ${theme.palette.divider}`,
+    },
+}));
+
+
+const Header = (() => {
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar 
+                position='static'
+                color='white'
+                elevation={0}
+                className={classes.appBar}  // connecting appBar
+            >
+                <Toolbar>
+                    <Typography varient='h6' color='inherit' noWrap>
+                        Blog
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    );
+});
+
+export default Header;
---
+++ react/blog_api/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+// import Footer from './components/Footer';
+
+
+const routing = (
+    <BrowserRouter>
+        <React.StrictMode>
+            <Header />
+            <Routes>
+                <Route exact path='/' component={App} />
+            </Routes>
+            {/* <Footer /> */}
+        </React.StrictMode>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
+reportWebVitals();
