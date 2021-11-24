922fd4d93be0f0da0ad1d0dd809b15b5f7fc4488
Khailas R <khailas303@gmail.com>
2021-11-24 14:29:53 +0530

    initial commit

---
+++ react/blog_api/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from './react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+const routing = (
+    <BrowserRouter>     
+        <React.StrictMode>  // Additional info for troubleshooting
+            <Header />
+            <Routes>
+                <Route exact path='/' component={App} />
+            </Routes>
+            <Footer />
+        </React.StrictMode>
+    </BrowserRouter>
+);
+
+reportWebVitals();
