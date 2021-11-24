bbce637ed28fae95f2572c38867e46de8fc33b44
Khailas R <khailas303@gmail.com>
2021-11-24 16:23:11 +0530

    footer added

--- react/blog_api/src/index.js
+++ react/blog_api/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 
 const routing = (
@@ -15,10 +15,11 @@ const routing = (
             <Routes>
                 <Route path='/' element={<App />} />
             </Routes>
-            {/* <Footer /> */}
+            <Footer />
         </React.StrictMode>
     </BrowserRouter>
 );
+
 
 ReactDOM.render(routing, document.getElementById('root'));
 reportWebVitals();
