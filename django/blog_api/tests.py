17be35e8184ac077ed08746779d4d1483df293d8
Khailas R <khailas303@gmail.com>
2021-11-27 19:03:43 +0530

    bug fix

--- django/blog_api/tests.py
+++ django/blog_api/tests.py
@@ -2,14 +2,11 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from blog.models import Category, Post
-from django.contrib.auth.models import User, UserManager
+from django.contrib.auth.models import User
 
 
 class PostTests(APITestCase):
-    
-    def setUp(self):
-        self.client = APIClient()
-    
+
     def test_view_posts(self):
         url = reverse('blog_api:listcreate')    # fetch info from blog_api urls.
         response = self.client.get(url, format='json')  # status code 200 is expected.
@@ -20,8 +17,8 @@ class PostTests(APITestCase):
     def test_create_post(self):
         self.test_category = Category.objects.create(name='main')
         
-        self.testuser1 = User.objects.create_user(
-            username='test_user', password='123456789'
+        self.test_user1 = User.objects.create_user(
+            username='test_user1', password='123456789'
         )
         
         data = {
@@ -58,7 +55,7 @@ class PostTests(APITestCase):
         )
         
         client.login(
-            username=self.test_user2.username, password='test1234'
+            username=self.test_user1.username, password='123456789'
         )
         
         url = reverse(
@@ -66,12 +63,14 @@ class PostTests(APITestCase):
         )
         
         response = client.put(
-            url, {
+            url,
+            {
                 "title": "New",
                 "author": 1,
                 "excerpt": "New",
                 "content": "New",
                 "status": "published"
-            }, format='json'
+            }, 
+            format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
