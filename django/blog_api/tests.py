2898c5da21763f7e307d1434a5960acce5796707
Khailas R <khailas303@gmail.com>
2021-11-27 20:26:04 +0530

    updated

--- django/blog_api/tests.py
+++ django/blog_api/tests.py
@@ -18,20 +18,21 @@ class PostTests(APITestCase):
         self.test_category = Category.objects.create(name='main')
         
         self.test_user1 = User.objects.create_user(
-            username='test_user1', password='123456789'
+            username='test_user_1', password='123456789'
         )
         
         data = {
             "title": "new",
             "author": 1,
             "excerpt": "new",
-            "content": "new"
+            "content": "new",
         }
         
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
     def test_post_update(self):
         client = APIClient()
