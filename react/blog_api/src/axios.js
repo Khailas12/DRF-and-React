1cdd96d7159e4a09912dd76585889ca8dc774746
Khailas R <khailas303@gmail.com>
2021-12-02 23:27:20 +0530

    updated

--- react/blog_api/src/axios.js
+++ react/blog_api/src/axios.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 
 const baseURL = 'http://127.0.0.1:8000/api/';
 
-
 const axiosInstance = axios.create({
     baseURL: baseURL,
     timeout: 5000,
+
     headers: {
+        // to get the data outta the local storage. 
         Authorization: localStorage.getItem('access_token')
             ? 'JWT ' + localStorage.getItem('access_token') : null,
         'Content-Type': 'application/json',
         accept: 'application/json'
     },
 });
+
