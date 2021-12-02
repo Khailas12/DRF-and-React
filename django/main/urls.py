f0383bfef3bf7feb81a9644bdbc330e3611ffcff
Khailas R <khailas303@gmail.com>
2021-12-02 20:34:40 +0530

    urls added and bugs fixed
    user created using postman

--- django/main/urls.py
+++ django/main/urls.py
@@ -8,6 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('api/', include('blog_api.urls', namespace='blog_api')),
+    path('api/user/', include('users.urls', namespace='users')),
     
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  
     
