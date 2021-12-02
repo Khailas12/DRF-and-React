565f661431cefc9fd5168e761aa1f91b37c01c8f
Khailas R <khailas303@gmail.com>
2021-12-02 20:23:48 +0530

    jwt on users
    initial

--- django/users/views.py
+++ django/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterUserSerializer
 
-# Create your views here.
+
+class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        req_serializer = RegisterUserSerializer(data=request.data)
+        
+        if req_serializer.is_valid():
+            new_user = req_serializer.save()
+            
+            if new_user:
+                return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(
+            req_serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
