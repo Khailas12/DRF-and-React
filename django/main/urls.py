2b88cb79782d188dfdc08d360920ca340f44dd2d
Khailas R <khailas303@gmail.com>
2021-12-10 20:49:54 +0530

    updated

--- django/main/urls.py
+++ django/main/urls.py
@@ -6,15 +6,15 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
-    path('', include('blog.urls', namespace='blog')),
+    # jwt 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # fetching or getting token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # reftreshing token
+    path('admin/', admin.site.urls),
+    
     path('api/', include('blog_api.urls', namespace='blog_api')),
     path('api/user/', include('users.urls', namespace='users')),    # registering user
     
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  
 
-    # jwt 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),    # fetching or getting token
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    # reftreshing token
-    
-    path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
 ]
