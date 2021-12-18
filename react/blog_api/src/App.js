4a9ffeb438ccbae7090779536c7b0043e052796b
Khailas R <khailas303@gmail.com>
2021-12-18 18:24:52 +0530

    updated

--- react/blog_api/src/App.js
+++ react/blog_api/src/App.js
@@ -26,7 +26,7 @@ const App = (() => {
         //         });
         //     });
 
-        // same as the commented one in fetched and used axios in here instead
+        // same functionality as fetch but used axios instead
         axios.get(apiUrl).then((posts) => {
             const allPosts = posts.data;
             setAppState({
@@ -44,5 +44,6 @@ const App = (() => {
         </div>
     );
 });
+
 
 export default App;
